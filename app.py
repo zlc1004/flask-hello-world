fea54679176dcdf0afb5bb78b8c49b29b13155e5
zlcython-by-zlc1003 <107649766+zlcython-by-zlc1003@users.noreply.github.com>
2024-09-26 10:11:56 -0700

    s

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+import subprocess
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/run')
+def run():
+    result = subprocess.run(['ls', '-l'], stdout=subprocess.PIPE)
+    return result.stdout
